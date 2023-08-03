9d2ab6e5546e6b59b643a8ba07efafb80750c519
sandy1110 <sandiig.1110@gmail.com>
2023-08-02 23:28:40 -0600

    landing page with testimonies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Testimonio } from './components/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1> Esto es lo que dicen nuestros clientes sobre nosotros</h1>
+        <Testimonio 
+          nombre='Sandra'
+          pais='Suecia'
+          imagen='sandy'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de SoftwareIngeniera de Software'
+        />
+        <Testimonio 
+          nombre='Iguano'
+          pais='Mexico'
+          imagen='pug'
+          cargo='Ingeniero de Software'
+          empresa='Youtube'
+          testimonio='Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de Software Ingeniera de SoftwareIngeniera de Software'
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Testimonio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles/Testimonio.css';
+
+export const Testimonio =  (props) => {
+    return (
+        <div className='contenedor-testimonio'>
+            <img className='imagen-testimonio' src={require(`../images/${props.imagen}.jpg`)} alt='Foto de pug' />
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>
+                    <strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className='cargo-testimonio'>
+                    {props.cargo} en <strong>{props.empresa}</strong></p>
+                <p className='texto-testimonio'>{props.testimonio}</p>
+            </div>
+        </div>
+    );
+}
